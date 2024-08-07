c49d15eb7997ecc91ffdf2c1aee26d0d248c5a85
carlsondultra <carlsondultra@users.noreply.github.com>
2024-08-06 22:43:11 -0400

    creating initial fabric workspace

--- src/features/editor/hooks/use-editor.ts
+++ src/features/editor/hooks/use-editor.ts
@@ -1,16 +1,37 @@
-import {fabric} from "fabric"
-import { useCallback } from "react"
+import { fabric } from "fabric";
+import { useCallback } from "react";
 
 export const useEditor = () => {
-    const init = useCallback(({
-        initialCanvas,
-        initialContainer,
+  const init = useCallback(
+    ({
+      initialCanvas,
+      initialContainer,
     }: {
-        initialCanvas: fabric.Canvas
-        initialContainer: HTMLDivElement,
+      initialCanvas: fabric.Canvas;
+      initialContainer: HTMLDivElement;
     }) => {
-        console.log("Initializing Editor")
-    }, [])
+        const initialWorkspace = new fabric.Rect({
+            width: 900,
+            height: 1200,
+            name: "clip",
+            fill: "white",
+            selectable: false,
+            hasControls: false,
+            shadow: new fabric.Shadow({
+                color: "rgba(0,0,0,0.8)",
+                blur: 5,
+            })
+        })
 
-    return { init }
-}
+      initialCanvas.setWidth(initialContainer.offsetWidth);
+      initialCanvas.setHeight(initialContainer.offsetHeight);
+
+      initialCanvas.add(initialWorkspace)
+      initialCanvas.centerObject(initialWorkspace) //centering workspace
+      initialCanvas.clipPath = initialWorkspace // every element not in central workspace will not be visible
+    },
+    []
+  );
+
+  return { init };
+};
