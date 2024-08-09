96f0df2bdbfea688b6b66597079a10ea505963a5
carlsondultra <carlsondultra@users.noreply.github.com>
2024-08-08 21:36:02 -0400

    creating sidebar component and adding onto editor

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -4,6 +4,7 @@ import { fabric } from "fabric";
 import { useEditor } from "@/features/editor/hooks/use-editor";
 import { useEffect, useRef } from "react";
 import { Navbar } from "./navbar";
+import { Sidebar } from "./sidebar";
 
 export const Editor = () => {
   const { init } = useEditor();
@@ -26,8 +27,11 @@ export const Editor = () => {
   return (
     <div className="h-full flex flex-col">
       <Navbar />
-      <div className="flex-1 h-full bg-muted" ref={containerRef}>
-        <canvas ref={canvasRef} />
+      <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+        <Sidebar />
+        <div className="flex-1 h-full bg-muted" ref={containerRef}>
+          <canvas ref={canvasRef} />
+        </div>
       </div>
     </div>
   );
