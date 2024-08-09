7aba79baf52270212bfc7ba648506be1c249552e
carlsondultra <carlsondultra@users.noreply.github.com>
2024-08-08 21:40:57 -0400

    creating toolbar component and adding onto editor

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -5,6 +5,7 @@ import { useEditor } from "@/features/editor/hooks/use-editor";
 import { useEffect, useRef } from "react";
 import { Navbar } from "./navbar";
 import { Sidebar } from "./sidebar";
+import { Toolbar } from "./toolbar";
 
 export const Editor = () => {
   const { init } = useEditor();
@@ -29,9 +30,12 @@ export const Editor = () => {
       <Navbar />
       <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
         <Sidebar />
-        <div className="flex-1 h-full bg-muted" ref={containerRef}>
-          <canvas ref={canvasRef} />
-        </div>
+        <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
+          <Toolbar />
+          <div className="flex-1 h-[calc(100%-124px)] bg-muted" ref={containerRef}>
+            <canvas ref={canvasRef} />
+          </div>
+        </main>
       </div>
     </div>
   );
