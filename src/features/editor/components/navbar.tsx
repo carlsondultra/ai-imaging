d750924fbc96447281258cb78e3fc18aebf64115
carlsondultra <carlsondultra@users.noreply.github.com>
2024-08-09 22:29:44 -0400

    adding dropdown menu to navbar

--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -1,11 +1,46 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { Logo } from "./logo"
+
+import {
+    DropdownMenu,
+    DropdownMenuItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { ChevronDown } from "lucide-react"
+import { CiFileOn } from "react-icons/ci"
 
 export const Navbar = () => {
     return (
         <nav className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">
             <Logo />
+            <div className="w-full flex items-center gap-x-1 h-full">
+                <DropdownMenu modal={false}>
+                    <DropdownMenuTrigger asChild>
+                        <Button size="sm" variant="ghost">
+                            File
+                            <ChevronDown className="size-4 ml-2" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="start" className="min-w-60">
+                        <DropdownMenuItem
+                        onClick={() => {}}
+                            className="flex items-center gap-x-2"
+                        >
+                            <CiFileOn className="size-8" />
+                            <div>
+                                <p>Open</p>
+                                <p className="text-xs text-muted-foreground">
+                                    Open a JSON file
+                                </p>
+                            </div>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+            </div>
         </nav>
     )
 }
