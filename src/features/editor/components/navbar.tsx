c68af74c80c16c5a0189a327f2b43cc096ef1684
carlsondultra <carlsondultra@users.noreply.github.com>
2024-08-10 21:58:49 -0400

    adding hint component onto navbar

--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -1,46 +1,53 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { Logo } from "./logo"
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
 
 import {
-    DropdownMenu,
-    DropdownMenuItem,
-    DropdownMenuContent,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { ChevronDown } from "lucide-react"
-import { CiFileOn } from "react-icons/ci"
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown, MousePointerClick } from "lucide-react";
+import { CiFileOn } from "react-icons/ci";
+import { Separator } from "@/components/ui/separator";
+import { Hint } from "@/components/hint";
 
 export const Navbar = () => {
-    return (
-        <nav className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">
-            <Logo />
-            <div className="w-full flex items-center gap-x-1 h-full">
-                <DropdownMenu modal={false}>
-                    <DropdownMenuTrigger asChild>
-                        <Button size="sm" variant="ghost">
-                            File
-                            <ChevronDown className="size-4 ml-2" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="start" className="min-w-60">
-                        <DropdownMenuItem
-                        onClick={() => {}}
-                            className="flex items-center gap-x-2"
-                        >
-                            <CiFileOn className="size-8" />
-                            <div>
-                                <p>Open</p>
-                                <p className="text-xs text-muted-foreground">
-                                    Open a JSON file
-                                </p>
-                            </div>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">
+      <Logo />
+      <div className="w-full flex items-center gap-x-1 h-full">
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger asChild>
+            <Button size="sm" variant="ghost">
+              File
+              <ChevronDown className="size-4 ml-2" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start" className="min-w-60">
+            <DropdownMenuItem
+              onClick={() => {}}
+              className="flex items-center gap-x-2"
+            >
+              <CiFileOn className="size-8" />
+              <div>
+                <p>Open</p>
+                <p className="text-xs text-muted-foreground">
+                  Open a JSON file
+                </p>
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Separator orientation="vertical" className="mx-2" />
+        <Hint label="Select" side="bottom" sideOffset={10}>
+          <Button variant="ghost" size="icon" onClick={() => {}} className="">
+            <MousePointerClick className="size-4" />
+          </Button>
+        </Hint>
+      </div>
+    </nav>
+  );
+};
