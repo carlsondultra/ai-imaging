509a3437676eeee1ade9f1e0206578dbe70da564
carlsondultra <carlsondultra@users.noreply.github.com>
2024-08-11 22:23:26 -0400

    setting active tool to whichever item the user clicks on

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,20 +2,40 @@
 
 import { fabric } from "fabric";
 import { useEditor } from "@/features/editor/hooks/use-editor";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Navbar } from "./navbar";
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
 import { Footer } from "./footer";
+import { ActiveTool } from "../types";
 
 export const Editor = () => {
+  const [activeTool, setActiveTool] = useState<ActiveTool>("select"); //defaulttool is select when using editor
+
+  const onChangeActiveTool = useCallback(
+    (tool: ActiveTool) => {
+      if (tool === activeTool) {
+        return setActiveTool("select");
+      }
+
+      if (tool === "draw") {
+      }
+
+      if (activeTool === "draw") {
+      }
+
+      setActiveTool(tool);
+    },
+    [activeTool]
+  );
+
   const { init } = useEditor();
 
   const canvasRef = useRef(null);
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    console.log("init")
+    console.log("init");
     const canvas = new fabric.Canvas(canvasRef.current, {
       controlsAboveOverlay: true,
       preserveObjectStacking: true,
@@ -28,18 +48,24 @@ export const Editor = () => {
 
     //unmounting elements
     return () => {
-      canvas.dispose()
-    }
+      canvas.dispose();
+    };
   }, [init]);
 
   return (
     <div className="h-full flex flex-col">
       <Navbar />
       <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
-        <Sidebar />
+        <Sidebar 
+        activeTool={activeTool}
+        onChangeActiveTool={onChangeActiveTool}
+        />
         <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
           <Toolbar />
-          <div className="flex-1 h-[calc(100%-124px)] bg-muted" ref={containerRef}>
+          <div
+            className="flex-1 h-[calc(100%-124px)] bg-muted"
+            ref={containerRef}
+          >
             <canvas ref={canvasRef} />
           </div>
           <Footer />
