cea55b65e180deaed0d74cea9734697483ee9c60
carlsondultra <carlsondultra@users.noreply.github.com>
2024-08-24 22:08:27 -0400

    adding toolbar props onto editor

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -70,7 +70,14 @@ export const Editor = () => {
           onChangeActiveTool={onChangeActiveTool}
         />
         <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
-          <Toolbar />
+          <Toolbar 
+            editor={editor}
+            activeTool={activeTool}
+            onChangeActiveTool={onChangeActiveTool}
+            key={JSON.stringify(
+              editor?.canvas.getActiveObject()
+            )}
+          />
           <div
             className="flex-1 h-[calc(100%-124px)] bg-muted"
             ref={containerRef}
