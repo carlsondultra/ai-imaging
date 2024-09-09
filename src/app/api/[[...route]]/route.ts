1276a9a1dec8eeff392eb9afee1428d6244fa5e2
carlsondultra <carlsondultra@users.noreply.github.com>
2024-09-08 22:34:53 -0400

    building hono rpc

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,13 +1,15 @@
 import { Hono } from "hono"
 import { handle } from "hono/vercel"
 
+import user from "./user"
+
 // use edge or nodejs (preference)
 export const runtime = "nodejs"
 
 const app = new Hono().basePath("/api")
 
-app.get("/test", (c) => {
-    return c.json({ test: "hono test"})
-})
+const routes = app.route("/user", user)
 
 export const GET = handle(app)
+
+export type AppType = typeof routes
