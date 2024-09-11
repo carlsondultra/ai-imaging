618a86c47121588fdd0e2151ed81a0f2eeb21780
carlsondultra <carlsondultra@users.noreply.github.com>
2024-09-11 16:03:12 -0400

    adding upload button functionality onto front-end

--- src/features/editor/components/image-sidebar.tsx
+++ src/features/editor/components/image-sidebar.tsx
@@ -4,9 +4,10 @@ import { ToolSidebarHeader } from "./tool-sidebar-header";
 import { ToolSidebarClose } from "./tool-sidebar-close";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useGetImages } from "@/features/images/api/use-get-images";
-import { AlertTriangle, Loader } from "lucide-react";
+import { AlertTriangle, Loader, Upload } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { UploadButton } from "@/lib/uploadthing";
 
 interface ImageSidebarProps {
   editor: Editor | undefined;
@@ -36,6 +37,22 @@ export const ImageSidebar = ({
         title="Images"
         description="Add images to your canvas"
       />
+      <div className="p-4 border-b">
+        <UploadButton 
+        appearance={{
+          button: "w-full text-sm font-medium",
+          allowedContent: "hidden"
+        }}
+        content={{
+          button: "Upload Image"
+        }}
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            editor?.addImage(res[0].url)
+          }}
+        />
+
+      </div>
       {isLoading && (
         <div className="flex items-center justify-center flex-1">
           <Loader className="size-4 text-muted-foreground animate-spin" />
