f5e7725b665d46b3c0a4b4333d797413203cad78
carlsondultra <carlsondultra@users.noreply.github.com>
2024-09-11 21:03:01 -0400

    creating api endpoint to generate ai images using replicate

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,14 +2,20 @@ import { Hono } from "hono"
 import { handle } from "hono/vercel"
 
 import images from "./images"
+import ai from "./ai"
 
 // use edge or nodejs (preference)
 export const runtime = "nodejs"
 
 const app = new Hono().basePath("/api")
 
-const routes = app.route("/images", images)
+const routes = app
+    .route("/ai", ai)
+    .route("/images", images)
 
 export const GET = handle(app)
+export const POST = handle(app)
+export const PATCH = handle(app)
+export const DELETE = handle(app)
 
 export type AppType = typeof routes
