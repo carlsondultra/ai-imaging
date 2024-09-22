fc916c7536709951a2579511bf34e4f9db6338c7
carlsondultra <carlsondultra@users.noreply.github.com>
2024-09-22 17:53:44 -0400

    implementing database connectivity using Drizzle ORM and Neon

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,13 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import {DrizzleAdapter} from "@auth/drizzle-adapter"
+
+import { db } from "./db/drizzle"
+import { accounts, users } from "./db/schema"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, signIn, signOut, auth } = 
+NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [GitHub],
 })
 
