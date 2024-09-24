29168f409c5f64df055df46519932f693780fb9d
carlsondultra <carlsondultra@users.noreply.github.com>
2024-09-24 19:17:27 -0400

    displaying ID in JWT session

--- src/auth.ts
+++ src/auth.ts
@@ -13,5 +13,24 @@ NextAuth({
     signIn: "/sign-in",
     error: "/sign-in"
   },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    session({ session, token }) {
+      if (token.id) {
+        session.user.id = token.id
+      }
+      
+      return session
+    },
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+
+      return token
+    }
+  },
 })
 
