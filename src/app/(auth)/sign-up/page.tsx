bb909031dc4f55a4dbe5930d7985767db45cff40
carlsondultra <carlsondultra@users.noreply.github.com>
2024-09-23 22:24:21 -0400

    adding sign-up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,16 +1,16 @@
 import { redirect } from "next/navigation";
 
 import { auth } from "@/auth";
-import { SignInCard } from "@/features/auth/components/sign-in-card";
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
 
-const SignInPage = async () => {
+const SignUpPage = async () => {
   const session = await auth();
 
   if (session) {
     redirect("/");
   }
 
-  return <SignInCard />
+  return <SignUpCard />
 };
 
-export default SignInPage;
+export default SignUpPage;
