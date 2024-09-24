34e2615d8142edfc832aa80594812d6b81e1ed22
carlsondultra <carlsondultra@users.noreply.github.com>
2024-09-24 19:08:49 -0400

    enabling log-in through Google

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,14 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 import {DrizzleAdapter} from "@auth/drizzle-adapter"
 
 import { db } from "./db/drizzle"
-import { accounts, users } from "./db/schema"
  
 export const { handlers, signIn, signOut, auth } = 
 NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [GitHub],
+  providers: [GitHub, Google],
   pages: {
     signIn: "/sign-in",
     error: "/sign-in"
