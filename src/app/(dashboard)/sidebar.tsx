57ecce01e3894d0cea4ad5a645f1977d6a5ba2a6
carlsondultra <carlsondultra@users.noreply.github.com>
2024-10-01 22:01:28 -0400

    beginning to create navbar and sidebar portions of dashboard

---
+++ src/app/(dashboard)/sidebar.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./logo"
+
+export const Sidebar = () => {
+    return (
+        <aside className="hidden lg:flex fixed flex-col w-[300px] left-0 shrink-0 h-full">
+            <Logo />
+        </aside>
+    )
+}
