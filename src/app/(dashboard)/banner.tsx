3113f7d80258cfa0dbeb044c03eb58d1081c4188
carlsondultra <carlsondultra@users.noreply.github.com>
2024-10-03 22:25:06 -0400

    making changes to allow banner to be responsive for mobile devices

--- src/app/(dashboard)/banner.tsx
+++ src/app/(dashboard)/banner.tsx
@@ -4,16 +4,16 @@ import { ArrowRight, Sparkles } from "lucide-react"
 export const Banner = () => {
     return (
         <div className="text-white aspect-[5/1] min-h-[248px] flex gap-x-6 p-6 items-center rounded-xl bg-gradient-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5]">
-            <div className="rounded-full size-28 flex items-center justify-center bg-white/50">
+            <div className="rounded-full size-28 flex items-center justify-center bg-white/50 hidden md:flex">
                 <div className="rounded-full size-20 flex items-center justify-center bg-white">
                     <Sparkles className="h-20 text-[#0073ff] fill-[#0073ff]" />
                 </div>
             </div>
                 <div className="flex flex-col gap-y-2">
-                    <h1 className="text-3xl font-semibold">
+                    <h1 className="text-xl md:text-3xl font-semibold">
                         Visualize your ideas with AI Imaging
                     </h1>
-                    <p className="text-sm mb-2">
+                    <p className="text-xs md:text-sm mb-2">
                         Turn inspiration into design in no time. Simply upload an image and let AI do the rest.
                     </p>
                     <Button 
