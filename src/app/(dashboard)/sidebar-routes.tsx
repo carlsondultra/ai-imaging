0876f3d7ab9254a52fa8da099474228a9cddf72e
carlsondultra <carlsondultra@users.noreply.github.com>
2024-10-03 21:42:00 -0400

    adding sidebar items to list

--- src/app/(dashboard)/sidebar-routes.tsx
+++ src/app/(dashboard)/sidebar-routes.tsx
@@ -2,10 +2,14 @@
 
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import { Crown, Home } from "lucide-react"
+import { CreditCard, Crown, Home, MessageCircleQuestion } from "lucide-react"
 import { SidebarItem } from "./sidebar-item"
+import { usePathname } from "next/navigation"
 
 export const SidebarRoutes = () => {
+    const pathname = usePathname()
+
+
     return (
         <div className="flex flex-col gap-y-4 flex-1">
             <div className="px-4">
@@ -27,6 +31,23 @@ export const SidebarRoutes = () => {
                     href="/"
                     icon={Home}
                     label="Home"
+                    isActive={pathname === "/"}
+                />
+            </ul>
+            <div className="px-3">
+                <Separator />
+            </div>
+            <ul className="flex flex-col gap-y-1 px-3">
+                <SidebarItem 
+                    href={pathname}
+                    icon={CreditCard}
+                    label="Billing"
+                    onClick={() => {}}
+                />
+                <SidebarItem 
+                    href="mailto:imaging@gmail.com"
+                    icon={MessageCircleQuestion}
+                    label="Get Help"
                 />
             </ul>
         </div>
