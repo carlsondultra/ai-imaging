688cdf6d6963738edf6e3618ba8052ef9065f579
carlsondultra <carlsondultra@users.noreply.github.com>
2024-10-05 22:27:15 -0400

    updating database schema to include created projects

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm"
 import {
     boolean,
     timestamp,
@@ -20,6 +21,10 @@ import {
     image: text("image"),
     password: text("password"),
   })
+
+  export const usersRelations = relations(users, ({ many }) => ({
+    projects: many(projects),
+  }))
    
   export const accounts = pgTable(
     "account",
@@ -87,3 +92,30 @@ import {
       }),
     })
   )
+
+  export const projects = pgTable("project", {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+      name: text("name").notNull(),
+      userId: text("userId")
+      .notNull()
+      .references(() => users.id, {
+        onDelete: "cascade",
+      }),
+      json: text("json").notNull(),
+      height: integer("height").notNull(),
+      width: integer("width").notNull(),
+      thumbnailUrl: text("thumbNailUrl"),
+      isTemplate: boolean("isTemplate"),
+      isPro: boolean('isPro'),
+      createdAt: timestamp("createdAt", {mode: "date"}).notNull(),
+      updatedAt: timestamp("updatedAt", { mode: "date"}).notNull(),
+  })
+
+  export const projectsRelations = relations(projects, ({one}) => ({
+    user: one(users, {
+      fields: [projects.userId],
+      references: [users.id],
+    }),
+  }))
