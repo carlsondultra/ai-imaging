0261f1281650a776ba25319926412bc00562f78a
carlsondultra <carlsondultra@users.noreply.github.com>
2024-10-07 22:03:04 -0400

    adding mutation to create projects onto api

---
+++ src/features/projects/api/use-create-project.ts
@@ -0,0 +1,36 @@
+import { useMutation } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.projects["$post"], 200>
+type RequestType = InferRequestType<typeof client.api.projects["$post"]>["json"]
+
+export const useCreateProject = () => {
+    const mutation = useMutation<
+        ResponseType,
+        Error,
+        RequestType
+    >({
+        mutationFn: async (json) => {
+            const response = await client.api.projects.$post({json})
+
+            if (!response.ok) {
+                throw new Error("Something went wrong")
+            }
+
+            return await response.json()
+        },
+        onSuccess: () => {
+            toast.success("Project created")
+
+
+        },
+        onError: () => {
+            toast.error("Failed to create project")
+        }
+    })
+    return mutation
+}
+
