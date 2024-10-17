52dc32dc226f875afdac32d9e3784fa25d99e927
carlsondultra <carlsondultra@users.noreply.github.com>
2024-10-16 22:49:29 -0400

    handling error and pending statuses when getting projects

--- src/app/(dashboard)/projects-section.tsx
+++ src/app/(dashboard)/projects-section.tsx
@@ -1,9 +1,73 @@
 "use client"
 
+import { Table, TableRow, TableBody, TableCell } from "@/components/ui/table"
+import { useGetProjects } from "@/features/projects/api/use-get-projects"
+import { AlertTriangle, Loader, Search } from "lucide-react"
+
 export const ProjectsSection = () => {
+    const {
+        data,
+        status,
+        fetchNextPage,
+        isFetchingNextPage,
+        hasNextPage,
+    } = useGetProjects()
+
+    if (status === "pending"){
+        return (
+            <div className="space-y-4">
+            <h3 className="font-semibold text-lg">
+            Recent projects
+            </h3>
+            <div className="flex flex-col gap-y-4 items-center justify-center h-32">
+                <Loader className="size-6 text-muted-foreground animate-spin"/>
+            </div>
+        </div>
+        )
+    }
+
+    if (status === "error") {
+        return (
+            <div className="space-y-4">
+            <h3 className="font-semibold text-lg">
+            Recent projects
+            </h3>
+            <div className="flex flex-col gap-y-4 items-center justify-center h-32">
+                <AlertTriangle className="size-6 text-muted-foreground"/>
+                <p className="text-muted-foreground text-sm">
+                    Failed to load projects
+                </p>
+            </div>
+        </div>
+        )
+    }
+
+    if (!data.pages.length) {
+        return (
+            <div className="space-y-4">
+            <h3 className="font-semibold text-lg">
+            Recent projects
+            </h3>
+            <div className="flex flex-col gap-y-4 items-center justify-center h-32">
+                <Search className="size-6 text-muted-foreground"/>
+                <p className="text-muted-foreground text-sm">
+                    No projects found
+                </p>
+            </div>
+        </div>
+        )
+    }
+
     return (
-        <div>
-            Projects
+        <div className="space-y-4">
+            <h3 className="font-semibold text-lg">
+            Recent projects
+            </h3>
+            <Table>
+                <TableBody>
+
+                </TableBody>
+            </Table>
         </div>
     )
 }
