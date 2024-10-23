d08675d6e803a1f4f98402422c321dbb847c1598
carlsondultra <carlsondultra@users.noreply.github.com>
2024-10-22 22:51:11 -0400

    immediately updating and displaying changes under dashboard once refreshed

--- src/features/projects/api/use-update-project.ts
+++ src/features/projects/api/use-update-project.ts
@@ -29,6 +29,7 @@ export const useUpdateProject = (id: string) => {
             return await response.json()
         },
         onSuccess: () => {
+           queryClient.invalidateQueries({ queryKey: ["projects"] })
            queryClient.invalidateQueries({ queryKey: ["project", { id }] })
         },
         onError: () => {
