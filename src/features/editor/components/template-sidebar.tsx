76246ad6cc5240022e22f75b8b049709a4ee49c9
carlsondultra <carlsondultra@users.noreply.github.com>
2024-10-27 22:47:52 -0400

    creating template sidebar on opened projects

---
+++ src/features/editor/components/template-sidebar.tsx
@@ -0,0 +1,91 @@
+import { cn } from "@/lib/utils";
+import { ActiveTool, Editor } from "../types";
+import { ToolSidebarHeader } from "./tool-sidebar-header";
+import { ToolSidebarClose } from "./tool-sidebar-close";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useGetImages } from "@/features/images/api/use-get-images";
+import { AlertTriangle, Loader, Upload } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { UploadButton } from "@/lib/uploadthing";
+import { useGetTemplates } from "@/features/projects/api/use-get-templates";
+
+interface TemplateSidebarProps {
+  editor: Editor | undefined;
+  activeTool: ActiveTool;
+  onChangeActiveTool: (tool: ActiveTool) => void;
+}
+
+export const TemplateSidebar = ({
+  editor,
+  activeTool,
+  onChangeActiveTool,
+}: TemplateSidebarProps) => {
+  const { data, isLoading, isError } = useGetTemplates({
+    limit: "20",
+    page: "1",
+  });
+
+  const onClose = () => {
+    onChangeActiveTool("select");
+  };
+
+  return (
+    <aside
+      className={cn(
+        "bg-white relative border-r z-[40] w-[360px] h-full flex flex-col",
+        activeTool === "templates" ? "visible" : "hidden"
+      )}
+    >
+      <ToolSidebarHeader
+        title="Templates"
+        description="Choose from a variety of templates"
+      />
+      {isLoading && (
+        <div className="flex items-center justify-center flex-1">
+          <Loader className="size-4 text-muted-foreground animate-spin" />
+        </div>
+      )}
+      {isError && (
+        <div className="flex flex-col gap-y-4 items-center justify-center flex-1">
+          <AlertTriangle className="size-4 text-muted-foreground" />
+          <p className="text-muted-foreground text-xs">
+            Failed to fetch templates
+          </p>
+        </div>
+      )}
+      <ScrollArea>
+        <div className="p-4">
+          <div className="grid grid-cols-2 gap-4">
+            {data &&
+              data.map((template) => {
+                return (
+                  <button
+                  style={{ aspectRatio: `${template.width}/${template.height}`}}
+                    onClick={() => {}}
+                    key={template.id}
+                    className="relative w-full h-[100px] group hover:opacity-75 transition bg-muted rounded-sm overflow-hidden border"
+                  >
+                    <Image
+                      fill
+                      src={template.thumbnailUrl || ""}
+                      alt={template.name || "Template"}
+                      className="object-cover"
+                    />
+                    <div
+                      className="opacity-0  group-hover:opacity-100 
+                  absolute left-0 bottom-0 w-full text-[10px] truncate text-white p-1 bg-black/50 text-left"
+                    >
+                      {template.name}
+                    </div>
+                  </button>
+                );
+              })}
+          </div>
+        </div>
+      </ScrollArea>
+
+      <ToolSidebarClose onClick={onClose} />
+    </aside>
+  );
+};
