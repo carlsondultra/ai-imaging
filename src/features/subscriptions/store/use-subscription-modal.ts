634de60ada4aecb8ae81f8a132c94946384428fa
carlsondultra <carlsondultra@users.noreply.github.com>
2024-10-28 20:47:12 -0400

    adding subscription modal state using zustand

---
+++ src/features/subscriptions/store/use-subscription-modal.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand"
+
+type SubscriptionModalState = {
+    isOpen: boolean
+    onOpen: () => void
+    onClose: () => void
+}
+
+export const useSubscriptionModal = create<SubscriptionModalState>
+((set) => ({
+    isOpen: true,
+    onOpen: () => set({ isOpen:  true}),
+    onClose: () => set({ isOpen: false }),
+}))
